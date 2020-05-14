3b320894cab699ab13eaf09cf32edb40ed4ed673
jhonnet31 <jhon_gp@outlook.com>
2020-05-14 18:59:31 -0500

    agregando chart js

--- crypto/src/app/components/crypto/crypto.component.ts
+++ crypto/src/app/components/crypto/crypto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,ElementRef } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-crypto',
@@ -7,10 +8,55 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CryptoComponent implements OnInit {
 @Input() name:string;
+@Input() clave:string;
 
-  constructor() { }
+chart:any=null;
+
+  constructor(private elementRef:ElementRef) { }
 
   ngOnInit() {
+    let htmlRef = this.elementRef.nativeElement.querySelector("#"+this.clave);
+
+    this.chart = new Chart("hola", {
+      type: 'line',
+      data: {
+       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+       datasets: [
+         {
+        label: 'Data',
+        fill: false,
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: '#168ede',
+        borderColor: '#168ede'
+         }
+       ]
+        },
+        options: {
+       tooltips: {
+        enabled: false
+       },
+       legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+         fontColor: 'white'
+        }
+       },
+       scales: {
+         yAxes: [{
+          ticks: {
+           fontColor: "white"
+          }
+         }],
+         xAxes: [{
+        ticks: {
+         fontColor: "white",
+         beginAtZero: true
+        }
+         }]
+       }
+        }
+     });
   }
 
 }
